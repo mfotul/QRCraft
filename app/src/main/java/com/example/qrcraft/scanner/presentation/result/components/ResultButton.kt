efe2eb530de82580f4f179c476a85adf67f93f93
Marian Fotul <marian.fotul@swan.sk>
2025-08-26 10:40:52 +0200

    Milestone #1

--- app/src/main/java/com/example/qrcraft/scanner/presentation/result/components/ResultButton.kt
+++ app/src/main/java/com/example/qrcraft/scanner/presentation/result/components/ResultButton.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -13,13 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.qrcraft.R
 import com.example.qrcraft.ui.theme.QRCraftTheme
 
@@ -32,10 +30,9 @@ fun ResultButton(
 ) {
     Button(
         onClick = onClick,
-        shape = RectangleShape,
+        shape = RoundedCornerShape(100.dp),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.surfaceVariant
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
         ),
         modifier = modifier
     ) {
@@ -51,9 +48,10 @@ fun ResultButton(
             )
             Text(
                 text = stringResource(text),
-                style = MaterialTheme.typography.labelLarge,
+                style = MaterialTheme.typography.labelLarge.copy(
+                    letterSpacing = MaterialTheme.typography.labelLarge.fontSize * 0.01f
+                ),
                 color = MaterialTheme.colorScheme.onSurface,
-                letterSpacing = (-1).sp,
             )
         }
 
@@ -61,7 +59,7 @@ fun ResultButton(
 }
 
 @Composable
-@Preview(name = "Share")
+@Preview(name = "Share", showBackground = true)
 fun ResultShareButtonPreview() {
     QRCraftTheme {
         ResultButton(
@@ -73,7 +71,7 @@ fun ResultShareButtonPreview() {
 }
 
 @Composable
-@Preview(name = "Copy")
+@Preview(name = "Copy", showBackground = true)
 fun ResultCopyButtonPreview() {
     QRCraftTheme {
         ResultButton(
